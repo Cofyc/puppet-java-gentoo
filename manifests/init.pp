e22be9159a15076e140670b8ebb517622eb8ccec
Yecheng Fu <cofyc.jackson@gmail.com>
2013-07-25 14:00:42 +0800

    add new support version: 1.7.0.25

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,11 +5,13 @@ class java (
   $version = "1.7.0.21",
   $source = "puppet:///files/distfiles/jre-7u21-linux-x64.tar.gz",
 ) {
+  $distfile = "";
   case $version {
     '1.7.0.21': {
-      file { "/usr/portage/distfiles/jre-7u21-linux-x64.tar.gz":
-        source => $source
-      }
+      $distfile = "jre-7u21-linux-x64.tar.gz",
+    }
+    '1.7.0.25': {
+      $distfile = "jre-7u25-linux-x64.tar.gz",
     }
     default: {
       fail("unsupported version ${version}")
@@ -22,6 +24,9 @@ class java (
     fail("Module is not compatible with ${::operatingsystem}")
   }
 
+  file { "/usr/portage/distfiles/${distfile}":
+    source => $source
+  } ->
   portage::package { 'dev-java/oracle-jre-bin':
     ensure           => $version,
     keywords         => ['~amd64'],
